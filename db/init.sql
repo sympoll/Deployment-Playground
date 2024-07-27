5c76d3a15bdd49856d5a74f364bcea72952826ba
Ronen Gelmanovich <ronenge@mta.ac.il>
2024-07-27 14:40:35 +0300

    Try to deploy frontend, poll managment and a simple mock db

---
+++ db/init.sql
@@ -0,0 +1,22 @@
+-- Poll Management Service Schema
+CREATE TABLE
+    polls (
+        poll_id uuid PRIMARY KEY,
+        title VARCHAR(255),
+        description TEXT,
+        num_answers_allowed INT,
+        creator_id INT,
+        group_id INT,
+        time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        time_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        deadline TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE
+    answer_options (
+        answer_id SERIAL PRIMARY KEY,
+        poll_id uuid REFERENCES polls (poll_id),
+        ordinal INT,
+        answer_text TEXT,
+        num_of_votes INT
+    );
