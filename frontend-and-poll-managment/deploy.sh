448f58de25e9557722ebf62f26cc5457f2f176fe
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-07-31 10:30:07 +0300

    Added resource limits, created deployment and service files for the API gateway.

--- frontend-and-poll-managment/deploy.sh
+++ frontend-and-poll-managment/deploy.sh
@@ -4,11 +4,17 @@
 echo "Applying ConfigMap..."
 kubectl apply -f init-configmap.yml
 
-# Apply Backend Deployment and Service
-echo "Applying Backend Deployment..."
-kubectl apply -f backend-deployment.yml
-echo "Applying Backend Service..."
-kubectl apply -f backend-service.yml
+# Apply API Gateway Deployment and Service
+echo "Applying API gateway Deployment..."
+kubectl apply -f api-gateway-deployment.yml
+echo "Applying API gateway Service..."
+kubectl apply -f api-gateway-service.yml
+
+# Apply Poll Manager Deployment and Service
+echo "Applying Poll Manager Deployment..."
+kubectl apply -f poll-manager-deployment.yml
+echo "Applying Poll Manager Service..."
+kubectl apply -f poll-manager-service.yml
 
 # Apply Frontend Deployment and Service
 echo "Applying Frontend Deployment..."
@@ -22,6 +28,10 @@ kubectl apply -f postgres-deployment.yml
 echo "Applying Postgres Service..."
 kubectl apply -f postgres-service.yml
 
+# Apply console deployment
+echo Applying Console Deployment...
+kubectl apply -f console-deployment.yml
+
 # Wait for all pods to be in Running state
 echo "Waiting for all pods to be in Running state..."
 while [[ $(kubectl get pods --no-headers | grep -v Running | wc -l) -ne 0 ]]; do
@@ -31,6 +41,6 @@ done
 
 # Port forward the frontend service
 echo "Port forwarding frontend service..."
-kubectl port-forward svc/frontend 8080:8080
+kubectl port-forward svc/frontend 8080:80
 
 echo "Deployment and port forwarding complete."
